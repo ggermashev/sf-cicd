25ecde8348aaff5b02fb7de933da97844c4a7d45
ggermashev <ggermashev@gmail.com>
2024-12-03 21:25:17 +0300

    fix build

--- src/App.js
+++ src/App.js
@@ -9,14 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p
         >
           CI/CD Works!
-        </a>
+        </p>
       </header>
     </div>
   );
