6ee87e9fa9279ee18f8a742f1f17cdd9ed130dc9
ggermashev <ggermashev@gmail.com>
2024-12-03 21:19:02 +0300

    cicd works

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!!!!
+          CI/CD Works!
         </a>
       </header>
     </div>
